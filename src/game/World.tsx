463f3929e04c71b06e232c0aa67f4589c4c4e37c
Sour Paint studios <sourpaintstudios@gmail.com>
2026-09-08 14:17:56 -0700

    Update World.tsx

--- src/game/World.tsx
+++ src/game/World.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+8import { useEffect, useRef } from "react";
 import * as THREE from "three";
 import { EffectComposer } from "three/addons/postprocessing/EffectComposer.js";
 import { OutputPass } from "three/addons/postprocessing/OutputPass.js";
